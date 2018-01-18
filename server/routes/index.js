43ad3b9ebfce3f593778f5d869b1010d1acc9da0
Sherwin Mina <sherwinmina@yahoo.com>
2018-01-18 12:40:31 -0800

    Added Madhu

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,12 @@ var router = express.Router()
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.json([{ name: 'Sherwin' }, { name: 'Shav' }, { name: 'Christian' }])
+  res.json([
+    { name: 'Sherwin' },
+    { name: 'Shav' },
+    { name: 'Christian' },
+    { name: 'Madhu' }
+  ])
 })
 
 module.exports = router
